c2d0f88ae0362ec8c1e5a25e9c605ff0630fd8e0
anonymous298 <tackletalha@gmail.com>
2026-03-05 14:50:06 +0500

    new changes

--- app/(patient)/page.tsx
+++ app/(patient)/page.tsx
@@ -1,7 +1,10 @@
+import CallToActionSection from "@/components/patient/Landing/CallToActionSection";
 import DoctorListingPreview from "@/components/patient/Landing/DoctorListingPreview";
 import FeaturesSection from "@/components/patient/Landing/FeaturesSection";
 import HeroSection from "@/components/patient/Landing/HeroSection";
 import HowItWorksSection from "@/components/patient/Landing/HowItWorksSection";
+import StatisticsSection from "@/components/patient/Landing/StatisticsSection";
+import TestimonialsSection from "@/components/patient/Landing/TestimonialsSection";
 
 
 export default function Home() {
@@ -21,7 +24,14 @@ export default function Home() {
         {/* How It Works Section for our Patient Landing Page */}
         <HowItWorksSection/>
 
-        
+        {/* Testimonial Section for our Patient Landing Page */}
+        <TestimonialsSection/>
+
+        {/* Statistics Section for Patient Landing Page */}
+        <StatisticsSection/>
+
+        {/* Call-To-Action for Patient Landing Page */}
+        <CallToActionSection/>
 
     </div>
   );
