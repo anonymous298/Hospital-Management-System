cb8a9a15929bb639a7e6d0ba80694999df594df4
anonymous298 <tackletalha@gmail.com>
2026-03-05 14:31:24 +0500

    Function created for checking and creating user based on Initial Authentication status

---
+++ server/actions/user.action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+const createInitialUserInDbOnSignIn = async () => {
+    try {
+        const {userId: clerkId} = await auth();
+        const user = await currentUser();
+
+        if (!clerkId) throw new Error("UnAuthenticated Authentication Required");
+
+        const exisitingUser = await prisma.user.findUnique({
+            where : {
+                clerkId
+            }
+        });
+
+        if (!exisitingUser) {
+            const newUser = await prisma.user.create({
+                data : {
+                    clerkId,
+                    email: (user?.emailAddresses[0].emailAddress) ?? "",
+                    username: (user?.username || user?.emailAddresses[0].emailAddress.split('@')[0]) ?? "",
+                    name: `${user?.firstName ?? ""} ${user?.lastName ?? ""}`,
+                    imageUrl: user?.imageUrl,
+
+                }
+            })
+        }
+
+    } catch (error) {
+        console.log("Error While Creating User")
+        throw new Error("Error While Creating User")
+    }
+}
