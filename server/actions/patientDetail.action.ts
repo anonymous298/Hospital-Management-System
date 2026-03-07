8fd526fac3ebf8cc7bd7234f4be0bc1435755fcc
anonymous298 <tackletalha@gmail.com>
2026-03-07 10:52:05 +0500

    More checks added DB User check added for more security

--- server/actions/patientDetail.action.ts
+++ server/actions/patientDetail.action.ts
@@ -4,6 +4,7 @@ import { GenderRole } from "@/app/generated/prisma/enums";
 import prisma from "@/lib/prisma";
 import { FormProp } from "@/types/patientDetail";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { getCurrentDbUserId } from "./user.action";
 
 
 
@@ -13,6 +14,9 @@ export async function createPatientDetail({form, appointmentId}: {form: FormProp
         const user = await currentUser();
 
         if (!user) throw new Error("UnAuthenticated User");
+
+        const currentUserDbId = await getCurrentDbUserId();
+        if (!currentUserDbId) throw new Error("User Not in DB");
 
         const patientDetail = await prisma.patientDetail.create({
             data : {
