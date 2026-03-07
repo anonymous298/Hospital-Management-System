729d279f0983c11d603d476fac9cc306bc9d817b
anonymous298 <tackletalha@gmail.com>
2026-03-07 09:29:04 +0500

    runs server action function and runs map and displays limited doctorscard via doctors data

--- components/patient/Landing/DoctorListingPreview.tsx
+++ components/patient/Landing/DoctorListingPreview.tsx
@@ -1,38 +1,68 @@
-import React from 'react'
+
+import { fetchLimitedDoctors } from '@/server/actions/doctor.seed'
 import DoctorCard from './DoctorCard'
+import { DoctorAvailabilityStatus } from '@/app/generated/prisma/enums'
 
-const dummyDoctors = [
-  {
-    name: 'Dr. Sarah Johnson',
-    specialization: 'Dentist',
-    experience: '6 Years',
-    consultationFee: 3000,
-    imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770477280/doctors/jfrdnomnt4yveeglqprc.jpg',
-  },
-  {
-    name: 'Dr. John Smith',
-    specialization: 'Cardiologist',
-    experience: '10 Years',
-    consultationFee: 5000,
-    imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770476988/doctors/h8lw6hkagtm8mnvtcr15.png',
-  },
-  {
-    name: 'Dr. Emily Davis',
-    specialization: 'Dermatologist',
-    experience: '5 Years',
-    consultationFee: 2500,
-    imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770477684/doctors/lf19aocaldc1whngvvbo.png',
-  },
-  {
-    name: 'Dr. Michael Lee',
-    specialization: 'Orthopedic',
-    experience: '8 Years',
-    consultationFee: 4000,
-    imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770476814/doctors/tnc1rxr9by6ugazabs9j.png',
-  },
-]
+// const dummyDoctors = [
+//   {
+//     id: '2dfsdff2f2fd9d9ds9f',
+//     name: 'Dr. Sarah Johnson',
+//     specialization: 'Dentist',
+//     experience: '6 Years',
+//     consultationFee: 3000,
+//     patients: '3,000+',
+//     success: '95%',
+//     imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770477280/doctors/jfrdnomnt4yveeglqprc.jpg',
+//     availability: DoctorAvailabilityStatus.AVAILABLE,
+//     location: 'Karachi',
+//     qualification: 'BDS, MDS - Prosthodontics',
+//   },
+//   {
+//     id: '9dfsdff2f2fd9d9ds9f',
+//     name: 'Dr. John Smith',
+//     specialization: 'Cardiologist',
+//     experience: '10 Years',
+//     consultationFee: 5000,
+//     patients: '10,000+',
+//     success: '98%',
+//     imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770476988/doctors/h8lw6hkagtm8mnvtcr15.png',
+//     availability: DoctorAvailabilityStatus.AVAILABLE,
+//     location: 'Islamabad',
+//     qualification: 'MBBS, MD - Cardiology',
+//   },
+//   {
+//     id: '5dfsdff2f2fd9d9ds9f',
+//     name: 'Dr. Emily Davis',
+//     specialization: 'Dermatologist',
+//     experience: '5 Years',
+//     consultationFee: 2500,
+//     patients: '2,000+',
+//     success: '92%',
+//     imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770477684/doctors/lf19aocaldc1whngvvbo.png',
+//     availability: DoctorAvailabilityStatus.AVAILABLE,
+//     location: 'Lahore',
+//     qualification: 'MBBS, MD - Dermatology',
+//   },
+//   {
+//     id: '1dfsdff2f2fd9d9ds9f',
+//     name: 'Dr. Michael Lee',
+//     specialization: 'Orthopedic',
+//     experience: '8 Years',
+//     consultationFee: 4000,
+//     patients: '5,000+',
+//     success: '96%',
+//     imageUrl: 'https://res.cloudinary.com/duplkba46/image/upload/v1770476814/doctors/tnc1rxr9by6ugazabs9j.png',
+//     availability: DoctorAvailabilityStatus.UNAVAILABLE,
+//     location: 'Multan',
+//     qualification: 'MBBS, MS - Orthopedics',
+//   },
+// ]
 
-const DoctorListingPreview = () => {
+const DoctorListingPreview = async () => {
+
+  // Fetching Limited Doctors Data From Doctor Table From DB
+  const doctors = await fetchLimitedDoctors();
+
   return (
     <section className="py-20 bg-[#F8FAFC]">
       <div className="container mx-auto px-6 text-center">
@@ -42,14 +72,20 @@ const DoctorListingPreview = () => {
         </p>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {dummyDoctors.map((doctor, idx) => (
+          {doctors.map((doctor, idx) => (
             <DoctorCard
               key={idx}
+              id={doctor.id}
               name={doctor.name}
               specialization={doctor.specialization}
               experience={doctor.experience}
               consultationFee={doctor.consultationFee}
-              imageUrl={doctor.imageUrl}
+              imageUrl={(doctor.imageUrl ?? "https://res.cloudinary.com/duplkba46/image/upload/v1770477280/doctors/jfrdnomnt4yveeglqprc.jpg")}
+              availability={doctor.availability}
+              patients={doctor.patients}
+              success={doctor.success}
+              location={doctor.location}
+              qualification={doctor.qualification}
             />
           ))}
         </div>
