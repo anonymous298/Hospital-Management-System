dc2d8a02e29d96dba9bfad9cdbd385daeb6e8363
anonymous298 <tackletalha@gmail.com>
2026-03-07 09:43:44 +0500

    server action function created for fetching Single Doctor for Dynamic Doctor Route

--- server/actions/doctor.seed.ts
+++ server/actions/doctor.seed.ts
@@ -3,6 +3,7 @@
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
+// Function for fetching Limited Doctors limit-4
 export async function fetchLimitedDoctors() {
     try {
         // const {userId: clerkId} = await auth();
@@ -22,6 +23,7 @@ export async function fetchLimitedDoctors() {
     }
 }
 
+// Function for fetching All Doctors
 export async function fetchAllDoctors() {
     try {
         const doctors = await prisma.doctor.findMany({});
@@ -32,5 +34,36 @@ export async function fetchAllDoctors() {
     } catch (error) {
         console.log("UnAble to fetch all doctors data", error);
         throw new Error("Unable to fetch all doctors data");
+    }
+}
+
+// Function for Fetching Specific Doctor Based on ID
+export async function fetchSingleDoctor(id: string) {
+    try {
+
+        if (!id) throw new Error("Id Not Recognized"); 
+
+        const doctorData = await prisma.doctor.findUnique({
+            where: {
+                id,
+            },
+
+            include : {
+                doctorAvailabilityDates : {
+                    include : {
+                        doctorTimeSlots : true,
+                    }
+                }
+            }
+        });
+
+        if (!doctorData) throw new Error("Doctor Data Not Found");
+
+        return doctorData;
+
+
+    } catch (error) {
+        console.log("Error getting Single Doctor Data", error);
+        throw new Error("Error getting Single Doctor Data");
     }
 }
