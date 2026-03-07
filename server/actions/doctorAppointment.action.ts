6145815a12919b5a4f9267b08bce15c5abac3ff8
anonymous298 <tackletalha@gmail.com>
2026-03-07 12:45:36 +0500

    Server Action Function created for Creating Appointment With Transction and Atomic Updates

---
+++ server/actions/doctorAppointment.action.ts
@@ -0,0 +1,91 @@
+"use server";
+
+import { DoctorAvailabilityStatus, GenderRole, PaymentMethod, PaymentStatus } from "@/app/generated/prisma/enums";
+import prisma from "@/lib/prisma";
+import { Doctor, DoctorTimeSlot } from "@/types/doctor";
+import { PatientFormData } from "@/types/patientDetail";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { getCurrentDbUserId } from "./user.action";
+
+
+// Function for creating Doctor Appointment
+export async function createDoctorAppointment({
+    doctor,
+    timeSlot,
+    paymentMethod,
+    paymentStatus,
+    patientForm,
+}: {
+    doctor: Doctor,
+    timeSlot: DoctorTimeSlot,
+    paymentMethod: PaymentMethod,
+    paymentStatus: PaymentStatus,
+    patientForm: PatientFormData
+}) {
+    try {
+        const {userId: clerkId} = await auth();
+
+        if (!clerkId) throw new Error("UnAuthenticated User");
+
+        const currentUserDbId = await getCurrentDbUserId();
+        if (!currentUserDbId) throw new Error("User Not in DB");
+
+        await prisma.$transaction(async (tx) => {
+            // Creating Appointment Based On User, Doctor, Timeslot
+            const currentAppointment = await tx.doctorAppointment.create({
+                data : {
+                    userId: currentUserDbId,
+                    doctorId: doctor.id,
+                    timeSlotId: timeSlot.id,
+                }
+            });
+
+            // Atomic Update
+            await prisma.doctorTimeSlot.update({
+                where : {
+                    id: timeSlot.id
+                },
+
+                data : {
+                    isBooked: true
+                }
+            })
+
+            console.log("Appointment Created...")
+            
+            // Creating Patient Detail Based On currentAppointment In Transaction
+            await tx.patientDetail.create({
+                data : {
+                    doctorAppointmentId: currentAppointment.id,
+                    fullName: patientForm.fullName,
+                    age: parseInt(patientForm.age),
+                    phoneNumber: patientForm.phoneNumber,
+                    gender: (patientForm.gender) as GenderRole,
+                    email: patientForm.email
+                }
+            });
+            
+            console.log("Patient Detail Created...")
+            
+            // Creating Payment Based On current Appointment In Transaction
+            await tx.payment.create({
+                data : {
+                    userId: currentUserDbId,
+                    doctorId: doctor.id,
+                    doctorAppointmentId: currentAppointment.id,
+                    amount: doctor.consultationFee,
+                    method: paymentMethod,
+                    status: paymentStatus 
+                }
+            })
+
+            console.log("Payment Created...")
+        })
+
+        return {success : true}
+
+    } catch (error) {
+        console.log("Error creating Doctor Appointment", error);
+        return {success : false}
+    }
+}
