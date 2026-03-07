19adf5ad5ce09d67a5b740b5515fd04f77a79979
anonymous298 <tackletalha@gmail.com>
2026-03-07 10:28:31 +0500

    server action function created for getting current user data and current user id from db

--- server/actions/user.action.ts
+++ server/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { useId } from "react";
 
 // Function for Creating User In DB On Initial SignIn
 export async function createInitialUserInDbOnSignIn() {
@@ -33,5 +34,47 @@ export async function createInitialUserInDbOnSignIn() {
     } catch (error) {
         console.log("Error While Creating User")
         throw new Error("Error While Creating User")
+    }
+}
+
+// Function for Getting Current User Data Based On ClerkId
+export async function getCurrentDbUser() {
+    try {
+        const {userId: clerkId} = await auth();
+        // const user = await currentUser();
+
+        if (!clerkId) throw new Error("UnAuthenticated User");
+
+        const currentUser = await prisma.user.findUnique({
+            where : {
+                clerkId
+            }
+        });
+
+        if (!currentUser) throw new Error("User Not Found In DB");
+
+        return currentUser;
+
+    } catch (error) {
+        console.log("Error Getting Current DB User Info", error);
+        throw new Error("Error Getting Current DB User Info");
+    }
+}
+
+// Function for Getting Current User Id
+export async function getCurrentDbUserId() {
+    try {
+        const {userId: clerkId} = await auth();
+
+        if (!clerkId) throw new Error("UnAuthenticated User");
+
+        const user = await getCurrentDbUser();
+        const userId = user.id;
+
+        return userId;
+
+    } catch (error) {
+        console.log("Error Getting Current DB User ID", error);
+        throw new Error("Error Getting Current DB User ID");
     }
 }
