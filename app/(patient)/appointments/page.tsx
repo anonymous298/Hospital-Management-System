c5e6b1fd71d01b49234f5fddd92814bb37d3aae0
anonymous298 <tackletalha@gmail.com>
2026-03-10 10:18:38 +0500

    check added to display NotLoggedIn component if the user is not logged in based on user object

--- app/(patient)/appointments/page.tsx
+++ app/(patient)/appointments/page.tsx
@@ -1,10 +1,16 @@
 export const dynamic = 'force-dynamic'
 
 import AllDoctorAppointments from "@/components/patient/DoctorAppointments/AllDoctorApointments";
+import NotLoggedIn from "@/components/patient/DoctorAppointments/NotLoggedIn";
 import { fetchAllCurrentUserDoctorAppointments } from "@/server/actions/doctorAppointment.action"
+import { currentUser } from "@clerk/nextjs/server";
 
 
 const Page = async () => {
+
+    const user = await currentUser();
+
+    if (!user) return <NotLoggedIn/>;
 
     // Fetching ALl Current User Doctor Appointments
     const doctorAppointments = await fetchAllCurrentUserDoctorAppointments();
