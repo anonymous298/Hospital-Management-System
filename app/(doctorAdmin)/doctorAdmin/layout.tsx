06303920d5448e4028a94e450d99e64773acd655
anonymous298 <tackletalha@gmail.com>
2026-03-16 22:57:38 +0500

    Check added for user.role=doctor and user.doctorId if user has none of these no entry in doctorAdmin

--- app/(doctorAdmin)/doctorAdmin/layout.tsx
+++ app/(doctorAdmin)/doctorAdmin/layout.tsx
@@ -4,20 +4,27 @@ import React from "react"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import DoctorAdminShell from "@/components/doctorAdmin/layout/DoctorAdminShell"
+import { getCurrentDbUser } from "@/server/actions/user.action"
 
 export default async function DoctorAdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const user = await currentUser()
-  if (!user) redirect("/")
+  const clerkUser = await currentUser()
+  if (!clerkUser) redirect("/");
+
+  const dbUser = await getCurrentDbUser();
+//   if (!dbUser.doctorId) redirect('/');
+    console.log(dbUser.doctorId)
+
+  if (dbUser.role !== 'DOCTOR') redirect('/');
 
   return (
     <DoctorAdminShell
-      userName={`${user.firstName ?? ""} ${user.lastName ?? ""}`.trim()}
-      userEmail={user.emailAddresses[0]?.emailAddress ?? ""}
-      userImage={user.imageUrl ?? ""}
+      userName={`${clerkUser.firstName ?? ""} ${clerkUser.lastName ?? ""}`.trim()}
+      userEmail={clerkUser.emailAddresses[0]?.emailAddress ?? ""}
+      userImage={clerkUser.imageUrl ?? ""}
     >
       {children}
     </DoctorAdminShell>
